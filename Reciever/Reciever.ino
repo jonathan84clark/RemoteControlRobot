98cdb7e88b30cdfcb4371ff049f1f01c1d13b817
Jonathan L Clark <jonathan84clark@yahoo.com>
2019-12-25 22:09:05 -0800

    Created code for the transmitter Added some support for the reciever.

--- Reciever/Reciever.ino
+++ Reciever/Reciever.ino
@@ -8,13 +8,28 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-RF24 radio(9, 10); // CE, CSN
+RF24 radio(8, 10); // CE, CSN
 const byte address[6] = "39421";
 byte data[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 void setup() 
 {
    Serial.begin(9600);
+
+   pinMode(9, OUTPUT);
+   pinMode(6, OUTPUT);
+
+   pinMode(3, OUTPUT);
+   pinMode(5, OUTPUT);
+
+   // Left Forward
+   //digitalWrite(9, HIGH);
+   //digitalWrite(6, LOW);
+
+   //
+   //digitalWrite(3, HIGH);
+   //digitalWrite(5, LOW);
+   
    radio.begin();
    radio.openReadingPipe(0, address);   //Setting the address at which we will receive the data
    radio.setPALevel(RF24_PA_MIN);       //You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
@@ -26,7 +41,31 @@ void loop()
    if (radio.available())              //Looking for the data.
    {
       radio.read(&data, sizeof(data));    //Reading the data
-      Serial.println(data[1]);
+      if (data[4] == 0 && data[5] > 50)
+      {
+         digitalWrite(3, HIGH);
+         digitalWrite(5, LOW);
+
+         digitalWrite(9, HIGH);
+         digitalWrite(6, LOW);
+      }
+      else if (data[4] == 1 && data[5] > 50)
+      {
+         digitalWrite(3, LOW);
+         digitalWrite(5, HIGH);
+
+         digitalWrite(9, LOW);
+         digitalWrite(6, HIGH);
+      }
+      else
+      {
+         digitalWrite(3, LOW);
+         digitalWrite(5, LOW);
+
+         digitalWrite(9, LOW);
+         digitalWrite(6, LOW);
+      }
+      
       delay(5);
    } 
 }
--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -4,6 +4,7 @@
 * and sends a heartbeat message to the reciever.
 * Author: Jonathan L Clark
 * Date: 12/22/2019
+* Update: 12/25/2019, Added full support for all buttons and systems
 **************************************************************/
 #include <SPI.h>
 #include <nRF24L01.h>
@@ -11,10 +12,30 @@
 RF24 radio(9, 10); // CE, CSN         
 const byte address[6] = "39421";     //Byte of array representing the address. This is the address where we will send the data. This should be same on the receiving side.
 int button_pin = 2;
-byte data[8] = {0, 12, 0, 0, 0, 0, 0, 0};
+int center_y = 0;
+int center_x = 0;
+byte buttonRegister = 0x00;
+byte data[8] = {0xC1, 0xE2, 0, 0, 0, 0, 0, 0};
 
 void setup() 
 {
+    Serial.begin(9600);
+    pinMode(A7, OUTPUT);
+    pinMode(A6, OUTPUT);
+    pinMode(A5, OUTPUT);
+    pinMode(A0, INPUT);
+    pinMode(A1, INPUT);
+    pinMode(A2, INPUT);
+    pinMode(2, INPUT_PULLUP);
+    pinMode(3, INPUT_PULLUP);
+    pinMode(4, INPUT_PULLUP);
+    pinMode(5, INPUT_PULLUP);
+    pinMode(6, INPUT_PULLUP);
+    digitalWrite(A7, LOW);
+    digitalWrite(A6, LOW);
+    digitalWrite(A5, LOW);
+    center_y = analogRead(A1);
+    center_x = analogRead(A0);
     radio.begin();                  //Starting the Wireless communication
     radio.openWritingPipe(address); //Setting the address where we will send the data
     radio.setPALevel(RF24_PA_MIN);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
@@ -22,6 +43,78 @@ void setup()
 }
 void loop()
 {
+    // Calculate joystick data
+    int joystickYMag = center_y - analogRead(A1);
+    int joystickXMag = center_x - analogRead(A0);
+    float joystickYAdjusted = (float)joystickYMag * 255.0 / (float)center_y;
+    float joystickXAdjusted = (float)joystickXMag * 255.0 / (float)center_x;
+    byte xDir = 0;
+    if (joystickXAdjusted < 0.0)
+    {
+       joystickXAdjusted *= -1.0;
+       xDir = 1;
+    }
+    byte yDir = 0;
+    if (joystickYAdjusted < 0.0)
+    {
+      joystickYAdjusted *= -1.0;
+      yDir = 1;
+    }
+    if (digitalRead(A2) == 1)
+    {
+       buttonRegister |= 0x01;
+    }
+    else
+    {
+       buttonRegister &= ~0x01;
+    }
+    if (digitalRead(2) == 0)
+    {
+       buttonRegister |= 0x02;
+    }
+    else
+    {
+       buttonRegister &= ~0x02;
+    }
+    if (digitalRead(3) == 0)
+    {
+       buttonRegister |= 0x04;
+    }
+    else
+    {
+       buttonRegister &= ~0x04;
+    }
+    if (digitalRead(4) == 0)
+    {
+       buttonRegister |= 0x08;
+    }
+    else
+    {
+       buttonRegister &= ~0x08;
+    }
+    if (digitalRead(5) == 0)
+    {
+       buttonRegister |= 0x10;
+    }
+    else
+    {
+       buttonRegister &= ~0x10;
+    }
+    if (digitalRead(6) == 0)
+    {
+       buttonRegister |= 0x20;
+    }
+    else
+    {
+       buttonRegister &= ~0x20;
+    }
+
+    // Now we packetize it
+    data[2] = xDir;
+    data[3] = (byte)joystickXAdjusted;
+    data[4] = yDir;
+    data[5] = (byte)joystickYAdjusted;
+    data[6] = buttonRegister;
     radio.write(&data, sizeof(data));  //Sending the message to receiver 
-    delay(1000);
+    delay(100);
 }
