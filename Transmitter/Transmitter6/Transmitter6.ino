bd7b54ec77d7b5bdbc319acf3f412248c8857d30
Jonathan L Clark <jonathan84clark@yahoo.com>
2024-11-23 07:17:27 -0800

    Added transmiter6 to the code

--- Transmitter/Transmitter6/Transmitter6.ino
+++ Transmitter/Transmitter6/Transmitter6.ino
@@ -1,5 +1,5 @@
 /***************************************************************
-* TRANSMITTER 5
+* TRANSMITTER 6
 * DESC: The transmitter code takes data from a joystick and buttons
 * and sends a heartbeat message to the reciever.
 * Author: Jonathan L Clark
@@ -14,33 +14,32 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-RF24 radio(9, 10); // CE, CSN
+RF24 radio(10, 9); // CE, CSN
             
 int center_y = 0;
 int center_x = 0;
 byte buttonRegister1 = 0x00;
 byte buttonRegister2 = 0x00;
-byte device_address = 45;
+byte device_address = 120;
 byte data[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-int pulse_counter = 0;
-boolean powerIsPulse = false;
 const byte address[6] = "99885";
 
 #define JOYSTICK_X   A0
 #define JOYSTICK_Y   A1
-#define JOYSTICK_BTN  4
-#define BTN_DOWN      2
+#define JOYSTICK_BTN  A5
+#define BTN_DOWN      4
 #define BTN_RIGHT     5
 #define BTN_UP        6
 #define BTN_MID       7
 #define BTN_LEFT      8
-#define POWER_PULSE   2
 
-#define CFG_BIT0 A7
-#define CFG_BIT1 A6
-#define CFG_BIT2 A5
-#define CFG_BIT3 A4
-#define CFG_BIT4 A3
+#define SYNC_BTN 2
+
+#define CFG_BIT0 A2
+#define CFG_BIT1 A3
+#define CFG_BIT2 A4
+
+#define LED_PIN 3
 
 #define SYSTEM_ID 0x4D // Indicates that this is the Jonathan remote control system
 
@@ -54,8 +53,6 @@ void setup()
     pinMode(CFG_BIT0, INPUT);
     pinMode(CFG_BIT1, INPUT);
     pinMode(CFG_BIT2, INPUT);
-    pinMode(CFG_BIT3, INPUT);
-    pinMode(CFG_BIT4, INPUT);
     pinMode(JOYSTICK_X, INPUT);
     pinMode(JOYSTICK_Y, INPUT);
     pinMode(JOYSTICK_BTN, INPUT_PULLUP);
@@ -64,6 +61,8 @@ void setup()
     pinMode(BTN_UP, INPUT_PULLUP);
     pinMode(BTN_MID, INPUT_PULLUP);
     pinMode(BTN_LEFT, INPUT_PULLUP);
+    pinMode(SYNC_BTN, INPUT_PULLUP);
+    pinMode(LED_PIN, OUTPUT);
 
 
     byte cfg_address = 0;
@@ -71,20 +70,16 @@ void setup()
     // Handle negative high values
     cfg_address = (unsigned int)(analogRead(CFG_BIT0) < 100) | (unsigned int)(analogRead(CFG_BIT1) < 100) << 1;
     unsigned int bitVal2 = ~digitalRead(CFG_BIT2) & 0x0001;
-    unsigned int bitVal3 = ~digitalRead(CFG_BIT3) & 0x0001;
-    unsigned int bitVal4 = ~digitalRead(CFG_BIT4) & 0x0001;
-    cfg_address |= bitVal2 << 2 | bitVal3 << 3 | bitVal4 << 4;
+    cfg_address |= bitVal2 << 2;
 
     // Handle positive high values
     cfg_address = (unsigned int)(analogRead(CFG_BIT0) > 1000) | (unsigned int)(analogRead(CFG_BIT1) > 1000) << 1;
-    cfg_address |= (unsigned int)digitalRead(CFG_BIT2) << 2 | (unsigned int)digitalRead(CFG_BIT3) << 3 | (unsigned int)digitalRead(CFG_BIT4) << 4;
+    cfg_address |= (unsigned int)digitalRead(CFG_BIT2) << 2;
     cfg_address = (~cfg_address & 0x1F);
 
     device_address += cfg_address;
     Serial.print("Device address: ");
     Serial.println(device_address);
-    pinMode(POWER_PULSE, OUTPUT);
-    digitalWrite(POWER_PULSE, HIGH);
     center_y = analogRead(JOYSTICK_Y);
     center_x = analogRead(JOYSTICK_X);
     radio.begin();                  //Starting the Wireless communication
@@ -142,20 +137,6 @@ void loop()
     {
       joystickYAdjusted = 255.0;
     }
-    // Hanlde pulsing the power supply
-    if (powerIsPulse && pulse_counter > 3)
-    {
-       powerIsPulse = false;
-       pulse_counter = 0;
-       digitalWrite(POWER_PULSE, HIGH);
-    }
-    else if (!powerIsPulse && pulse_counter > 40)
-    {
-       powerIsPulse = true;
-       pulse_counter = 0;
-       digitalWrite(POWER_PULSE, LOW);
-    }
-    pulse_counter++;
     
     // Now we packetize it
     data[0] = SYSTEM_ID;
